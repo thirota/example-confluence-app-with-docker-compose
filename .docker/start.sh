07b677ea9e1b10f08c9d167b79f189e27579d31d
Takayuki Hirota <2185938+thirota@users.noreply.github.com>
2025-11-20 23:55:00 +0900

    chore: remove unused plugin development files #16

--- .docker/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /opt/atlassian/confluence/confluence/WEB-INF/atlassian-bundled-plugins/
-cp /opt/quickreload.properties /var/atlassian/application-data/confluence/quickreload.properties
-cp /opt/quickreload-5.0.2.jar /opt/atlassian/confluence/confluence/WEB-INF/atlassian-bundled-plugins/quickreload-5.0.2.jar
-chown -R confluence:confluence /var/atlassian/application-data/confluence
-
-/entrypoint.py
